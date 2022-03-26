fbf473553f92c68548d52f0fd16df9103911d17d
Chimney <Sshfail11%>
2022-03-26 19:52:18 +0100

    Make a dataframe out of parts.

--- model/modules/baskets.py
+++ model/modules/baskets.py
@@ -1,5 +1,5 @@
 from model.modules.parts import Part
-from pprint import pprint
+import pandas as pd
 
 class Basket:
     def __init__(self, name):
@@ -9,10 +9,19 @@ class Basket:
     def add_part(self, part: Part):
         self.parts.append(part)
 
+    def make_df(self):
+        if self.parts == []:
+            self.df = pd.DataFrame(pd.Series(None, name=self.name))
+        d = {part.name: part.price for part in self.parts}
+        self.df = pd.DataFrame(pd.Series(d, name=self.name))
+
     def show(self):
         print(f"{self.name} basket:")
         for part in self.parts:
             print('\t', part)
+
+
+
 
 
 
