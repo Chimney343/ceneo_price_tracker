624d7026fd1e7cf914d81eae6c9c53f50762aab4
Chimney <Sshfail11%>
2022-03-26 19:52:45 +0100

    Prices are now automatically cast to floats.

--- model/modules/parts.py
+++ model/modules/parts.py
@@ -3,9 +3,12 @@ class Part:
         self.name = name
         self.shop = shop
         self.price_format = price_format
-        self.price = price
+        self.price = self.price_string_to_float(price)
         self.value = value
         self.penny = penny
+
+    def price_string_to_float(self, price):
+        return float(price.replace(',', '.').replace(' ', ''))
 
     def __repr__(self):
         return repr(f"{self.name} / @ {self.shop} / @ {self.price}")
