295cb52805ef1d47d3ffbc2e6253b3370f5f13e4
Chimney <Sshfail11%>
2022-04-03 19:12:30 +0200

    Added BaseScraper class and inheritance from that class as well as CategoryScraper class.

--- model/modules/scrapers.py
+++ model/modules/scrapers.py
@@ -1,3 +1,5 @@
+import logging
+import traceback
 from datetime import datetime
 from pathlib import Path
 from typing import List, Union
@@ -5,35 +7,70 @@ from typing import List, Union
 import pandas as pd
 from tqdm.autonotebook import tqdm
 
-from model.modules.page_readers import CeneoSummaryPageReader
+import SETTINGS
+from model.modules.page_readers import CategoryReader, ProductSetReader
+
+logger = logging.getLogger(__name__)
 
 
-class CeneoScraper:
-    def __init__(
-        self, ceneo_summaries: Union[str, List], output_folder: Path = Path.cwd()
-    ):
-        self.ceneo_summaries = ceneo_summaries
-        self.dfs = []
+class BaseScraper:
+    def __init__(self, output_name: str = None, output_folder: Path = Path.cwd()):
+        self.output_name = output_name
         self.output_folder = Path(output_folder)
         self.df = None
-
-    def read_summary(self, url):
-        summary = CeneoSummaryPageReader(url=url)
-        try:
-            summary.read_summary()
-            return summary.df
-        except:
-            return pd.DataFrame(None)
-
-    def _make_result_df(self):
-        result = pd.concat(self.dfs).sort_values(by="timestamp")
-        return result
 
     def save_result_df(self):
         path = Path(self.output_folder)
         path.mkdir(parents=True, exist_ok=True)
-        filename = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.pkl"
+        if self.output_name:
+            filename = f"{self.output_name}_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.pkl"
+        else:
+            filename = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.pkl"
         self.df.to_pickle(path / filename)
+
+
+class CategoryScraper(BaseScraper):
+    def __init__(self, url, category_name: str, output_folder: Path = Path.cwd()):
+        super().__init__(output_name=category_name, output_folder=output_folder)
+        self.url = url
+
+    def read_category(self, url):
+        reader = CategoryReader(url=url)
+        try:
+            reader.read()
+            return reader.df
+        except Exception as e:
+            traceback.print_exc()
+            return pd.DataFrame(None)
+
+    def _make_result_df(self, df):
+        df = df.rename(columns={df.columns[0]: 'price'}).sort_values(by='price', ascending=False)
+        return df
+
+    def run(self):
+        df = self.read_category(url=self.url)
+        self.df = self._make_result_df(df=df)
+        self.df = df
+        self.save_result_df()
+
+
+class ProductSetScraper(BaseScraper):
+    def __init__(self, ceneo_summaries: Union[str, List], output_folder: Path = Path.cwd()):
+        super().__init__(output_name='product_set', output_folder=output_folder)
+        self.ceneo_summaries = ceneo_summaries
+        self.dfs = []
+
+    def read_summary(self, url):
+        reader = ProductSetReader(url=url)
+        try:
+            reader.read()
+            return reader.df
+        except:
+            return pd.DataFrame(None)
+
+    def _make_result_df(self):
+        df = pd.concat(self.dfs).sort_values(by="timestamp")
+        return df
 
     def run(self):
         if isinstance(self.ceneo_summaries, List):
