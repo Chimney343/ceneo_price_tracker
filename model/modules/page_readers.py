a946d9f2c0dc85fdc1308648f0fe24374530f704
Chimney <Sshfail11%>
2022-04-03 19:11:15 +0200

    Worked finished on CategoryReader class.

--- model/modules/page_readers.py
+++ model/modules/page_readers.py
@@ -1,3 +1,6 @@
+import concurrent.futures
+import logging
+import traceback
 from datetime import datetime
 from functools import reduce
 
@@ -6,9 +9,13 @@ import pandas as pd
 import requests
 import validators
 from bs4 import BeautifulSoup
+from tqdm.autonotebook import tqdm
 
 from model.modules.baskets import Basket
 from model.modules.parts import Part
+
+logger = logging.getLogger(__name__)
+
 
 class BaseReader:
     def __init__(self, url):
@@ -20,15 +27,13 @@ class BaseReader:
         Gets HTTP response from url.
         :param url:
         """
-        self.response = requests.get(self.url)
+        self.response = requests.get(url)
 
     def parse_page(self, url=None):
         """
         Parses a webpage with BeatifoulSoup.
         :param url:
         """
-        if url == None:
-            url = self.url
         self._get_response(url)
         if self.response.status_code == 200:
             return BeautifulSoup(self.response.text, "html.parser")
@@ -38,50 +43,99 @@ class BaseReader:
             page = self.page
         return page.title
 
-class CeneoCategoryReader(BaseReader):
-    def __init__(self, main_page_url):
-        super().__init__(main_page_url)
+
+class CategoryReader(BaseReader):
+    def __init__(self, url):
+        super().__init__(url)
         self.timestamp = None
-        self.baskets = {}
+        self.basket = {}
         self.n_category_pages = None
         self.category_urls = None
 
-    def _find_n_category_pages(self, page):
+    def get_title(self, page):
+        return page.title.text.split('-')[0].strip()
+
+    def find_n_total_pages(self, page):
         n_pages = int(page.find('input', class_="js_pagination-top-input")['data-pagecount'])
         return n_pages
 
-    def _generate_category_urls(self, base_url, n_category_pages):
+    def generate_category_urls(self, base_url, n_category_pages):
         urls = [f"https://www.ceneo.pl/Karty_graficzne;0020-30-0-0-{i}.htm" for i in range(0, n_category_pages)]
         urls[0] = base_url
         return urls
 
-    def _find_starting_tag(self, main_content_span_tags):
+    def find_starting_tag(self, main_content_span_tags):
         for i, tag in enumerate(main_content_span_tags):
             if tag.find(text="Więcej produktów"):
                 return (i + 1, main_content_span_tags[i + 1])
 
-    def _find_product_tags(self, page):
+    def find_product_tags(self, page):
         main_content_span_tags = page.find_all('div')[0].find_all('div', class_='main-content')[0].find_all('span')
-        starting_tag = self._find_starting_tag(main_content_span_tags)[0]
+        starting_tag = self.find_starting_tag(main_content_span_tags)[0]
         return main_content_span_tags[starting_tag:]
 
-    def read_page_product_tags(self, tags):
+    def read_products(self, url):
+        parts = []
+        page = self.parse_page(url)
+        tags = self.find_product_tags(page)
         for i, tag in enumerate(tags):
             product_review_tag = tag.find('span', class_='prod-review__qo')
             if product_review_tag:
                 part_name = product_review_tag.find('a')['title'].split(' o ')[-1]
                 part_id = tags[i + 7]['data-pid']
                 part_price = tags[i + 11].text
-                # TODO: Make this function return something. xD
                 part = Part(name=part_name, price=part_price, part_id=part_id)
+                parts.append(part)
+        return parts
+
+    def make_df(self, basket=None) -> pd.DataFrame:
+        """
+        Make a dataframe from existing basket.
+        """
+        basket.make_df()
+        df = basket.df.copy()
+        return df
+
+    def _enhance_df(self, df):
+        """
+        Result dataframe feature engineering.
+        :param df:
+        :return:
+        """
+        df["timestamp"] = self.timestamp
+        return df
 
     def read(self):
+        # Staging.
         self.main_page = self.parse_page(url=self.url)
-        self.n_category_pages = self._find_n_category_pages(page=self.main_page)
-        self.category_urls = self._generate_category_urls(base_url=self.url, n_category_pages=self.n_category_pages)
-        self.product_tags = self._find_product_tags(page=self.main_page)
+        # Timestamp is set immediately after parsing main category url.
+        self.timestamp = datetime.now()
+        self.title = self.get_title(page=self.main_page)
+        self.n_category_pages = self.find_n_total_pages(page=self.main_page)
+        self.category_urls = self.generate_category_urls(base_url=self.url, n_category_pages=self.n_category_pages)
+        self.basket = Basket(name=self.title)
 
-class CeneoSummaryPageReader(BaseReader):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=32) as executor:
+            future_to_url = {executor.submit(self.read_products, url): url for url in self.category_urls}
+            for future in tqdm(concurrent.futures.as_completed(future_to_url), total=self.n_category_pages):
+                url = future_to_url[future]
+                logger.debug((f"{url} scraped."))
+                try:
+                    parts = future.result()
+                    for part in parts:
+                        self.basket.add_part(part)
+                except Exception as e:
+                    logger.critical(
+                        f"Page at {url} returned an unhandled exception during scraping attempt. \n---TRACEBACK---\n"
+                    )
+                    traceback.print_exc()
+
+        df = self.make_df(basket=self.basket)
+        df = self._enhance_df(df=df)
+        self.df = df
+
+
+class ProductSetReader(BaseReader):
     def __init__(self, url: str):
         super().__init__(url)
         self.timestamp = None
@@ -105,9 +159,7 @@ class CeneoSummaryPageReader(BaseReader):
                 part_name = tag.find("img", alt=True)["alt"]
                 part_id = int(tag.find("input")["value"].replace('"', ""))
                 part_name_to_id[part_name] = part_id
-        part_id_to_name = {
-            part_id: part_name for part_name, part_id in part_name_to_id.items()
-        }
+        part_id_to_name = {part_id: part_name for part_name, part_id in part_name_to_id.items()}
         return part_name_to_id, part_id_to_name
 
     def make_baskets(self):
@@ -131,9 +183,7 @@ class CeneoSummaryPageReader(BaseReader):
         """
         basket_name = basket_tag["class"][0]
         try:
-            basket_part_id = int(
-                basket_tag.find("a")["data-productid"].replace('"', "")
-            )
+            basket_part_id = int(basket_tag.find("a")["data-productid"].replace('"', ""))
         except (TypeError, KeyError):
             basket_part_id = None
         try:
@@ -141,14 +191,10 @@ class CeneoSummaryPageReader(BaseReader):
         except (TypeError, KeyError):
             basket_part_brand = None
         try:
-            basket_part_category = basket_tag.find("a")["data-gacategoryname"].replace(
-                '"', ""
-            )
+            basket_part_category = basket_tag.find("a")["data-gacategoryname"].replace('"', "")
         except (TypeError, KeyError):
             basket_part_category = None
-        basket_span_tag_keys = [
-            span["class"][0] for span in basket_tag.find_all("span")
-        ]
+        basket_span_tag_keys = [span["class"][0] for span in basket_tag.find_all("span")]
         basket_span_tag_values = [span.text for span in basket_tag.find_all("span")]
         return (
             basket_name,
@@ -235,18 +281,10 @@ class CeneoSummaryPageReader(BaseReader):
         :param df:
         :return:
         """
-        df["cheapest-shop"] = df.index.to_series().apply(
-            self.find_cheapest_offer, return_type="shop"
-        )
-        df["most-expensive-shop"] = df.index.to_series().apply(
-            self.find_most_expensive_offer, return_type="shop"
-        )
-        df["cheapest-offer"] = df.index.to_series().apply(
-            self.find_cheapest_offer, return_type="price"
-        )
-        df["most-expensive-offer"] = df.index.to_series().apply(
-            self.find_most_expensive_offer, return_type="price"
-        )
+        df["cheapest-shop"] = df.index.to_series().apply(self.find_cheapest_offer, return_type="shop")
+        df["most-expensive-shop"] = df.index.to_series().apply(self.find_most_expensive_offer, return_type="shop")
+        df["cheapest-offer"] = df.index.to_series().apply(self.find_cheapest_offer, return_type="price")
+        df["most-expensive-offer"] = df.index.to_series().apply(self.find_most_expensive_offer, return_type="price")
         df["timestamp"] = self.timestamp
         df["title"] = self.title
         return df
@@ -265,9 +303,7 @@ class CeneoSummaryPageReader(BaseReader):
 
         df = (
             reduce(
-                lambda left, right: pd.merge(
-                    left, right, left_index=True, right_index=True, how="outer"
-                ),
+                lambda left, right: pd.merge(left, right, left_index=True, right_index=True, how="outer"),
                 dfs,
             )
             .reset_index()
@@ -277,12 +313,12 @@ class CeneoSummaryPageReader(BaseReader):
         df = self._enhance_df(df)
         return df
 
-    def read_summary(self):
+    def read(self):
         """
         Main flow; parses the page and products, initializes and fills the baskets, makes the result dataframe.
         """
-        self.page = self.parse_page()
-        self.title = self.get_title()
+        self.page = self.parse_page(url=self.url)
+        self.title = self.get_title(page=self.page)
         # Timestamp is set immediately after url parsing.
         self.timestamp = datetime.now()
         # Collate data on products in summary.
