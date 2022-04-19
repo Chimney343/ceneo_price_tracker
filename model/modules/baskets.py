ea8350384d966c2822df66a91236083d9448d62f
Chimney <Sshfail11%>
2022-04-19 18:04:24 +0200

    Slight renaming from Parts to Products.

--- model/modules/baskets.py
+++ model/modules/baskets.py
@@ -1,26 +1,26 @@
 import pandas as pd
 
-from model.modules.parts import Part
+from model.modules.parts import Product
 
 
 class Basket:
     def __init__(self, name):
         self.name = name
-        self.parts = []
+        self.products = []
 
-    def add_part(self, part: Part):
-        self.parts.append(part)
+    def add_part(self, part: Product):
+        self.products.append(part)
 
     def make_df(self):
-        if self.parts == []:
+        if self.products == []:
             self.df = pd.DataFrame(pd.Series(None, name=self.name))
         else:
-            d = {part.name: part.__dict__ for part in self.parts}
+            d = {part.name: part.__dict__ for part in self.products}
             df = pd.DataFrame(data=d).T.drop(labels=['name'], axis=1)
             df['basket_name'] = self.name
             self.df = df
 
     def show(self):
         print(f"{self.name} basket:")
-        for part in self.parts:
+        for part in self.products:
             print("\t", part)
